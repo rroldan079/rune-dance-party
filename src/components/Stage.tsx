641394e1b43f92fb9636812f54193ce6e9a681f0
Maria Panagos <maria.pan0330@gmail.com>
2023-07-23 18:42:46 -0500

    stage with tailwind instead of css

--- src/components/Stage.tsx
+++ src/components/Stage.tsx
@@ -3,30 +3,44 @@ import "./Stage.css";
 import { StageCard } from "./StageCard";
 
 export const Stage: React.FC = () => {
-
   ////////////////////// MOVE THIS ELSEWHERE
   // the dance move picking logic should probably be somewhere else;
   // this placeholder makes different cards for each player with Rune
-  const [stageCards, setStageCards] = useState<string[]>([])
-  const possibleColors: string[] = useMemo(()=>['red','yellow','lime','blue','purple','magenta','white'],[])
+  const [stageCards, setStageCards] = useState<string[]>([]);
+  const possibleColors: string[] = useMemo(
+    () => ["red", "yellow", "lime", "blue", "purple", "magenta", "white"],
+    []
+  );
 
-  useEffect(()=>{
-    let newCards: string[] = []
-    for (let i = 0; i < 10; i++){
-      newCards.push(possibleColors[Math.floor(Math.random()*possibleColors.length)])
+  useEffect(() => {
+    let newCards: string[] = [];
+    for (let i = 0; i < 10; i++) {
+      newCards.push(
+        possibleColors[Math.floor(Math.random() * possibleColors.length)]
+      );
     }
-    setStageCards(newCards)
-  }, [])
+    setStageCards(newCards);
+  }, []);
   ////////////////////// MOVE THIS ELSEWHERE ^^^
 
   return (
     <>
-      <div className="stage">
-        <div className="active-card"></div>
-        <div className="deck">
+      <div
+        id="stage"
+        className="flex absolute p-2 bg-orange-600 w-screen rounded-b-lg"
+      >
+        <div
+          id="active-card"
+          className="mr-2 w-10 h-14 border-2 border-black border-dashed rounded-md"
+        ></div>
+        <div id="deck" className="flex relative">
           {stageCards.map((item, i) => (
-            <div key={`stage-cards-${i}`} >
-              <StageCard cardType={item} leftOffset={`${(i*10)+2}px`} z={`${stageCards.length-i}`} />
+            <div key={`stage-cards-${i}`}>
+              <StageCard
+                cardType={item}
+                leftOffset={`${i * 10 + 2}px`}
+                z={`${stageCards.length - i}`}
+              />
             </div>
           ))}
         </div>
--- src/components/StageCard.tsx
+++ src/components/StageCard.tsx
@@ -4,10 +4,20 @@ type StageCardProps = {
   cardType: string;
   leftOffset: string;
   z: string;
-}
+};
 
-export const StageCard: React.FC<StageCardProps> = ({cardType, leftOffset, z}) => {
-  return (<>
-    <div className="stage-card" style={{ backgroundColor: cardType, left:leftOffset, zIndex: z}}></div>
-  </>);
+export const StageCard: React.FC<StageCardProps> = ({
+  cardType,
+  leftOffset,
+  z,
+}) => {
+  return (
+    <>
+      <div
+        id="stage-card"
+        className="absolute w-10 h-14 border-2 border-black rounded-md"
+        style={{ backgroundColor: cardType, left: leftOffset, zIndex: z }}
+      ></div>
+    </>
+  );
 };
