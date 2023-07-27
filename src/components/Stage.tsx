7632bd59e2942b39ed5818d9b4fda98640ea570e
Maria Panagos <maria.pan0330@gmail.com>
2023-07-26 20:16:03 -0500

    fixed card pulling order and added color scheme

--- src/components/Stage.tsx
+++ src/components/Stage.tsx
@@ -1,56 +1,63 @@
 import React, { useState, useEffect } from "react";
 // import "./Stage.css";
 import { StageCard } from "./StageCard";
-import { StageProps, Card } from "../types/types"
+import { StageProps, Card } from "../types/types";
 
 export const Stage: React.FC<StageProps> = ({ game }) => {
-    // TODO: Check the player poses when the card reaches its final form
-    // look into Intersection Observer API (tracks where an object is on the screen)
-    const [stageCards, setStageCards] = useState<Card[]>(game.newGame.cardStack);
-    const [activeCard, setActiveCard] = useState<Card>(game.newGame.cardStack[0]);
+  // TODO: Check the player poses when the card reaches its final form
+  // look into Intersection Observer API (tracks where an object is on the screen)
+  const [stageCards, setStageCards] = useState<Card[]>(game.newGame.cardStack);
+  const [activeCard, setActiveCard] = useState<Card>();
 
-    useEffect(() => {
-        console.log(game)
-    }, []);
+  useEffect(() => {
+    console.log(game);
+  }, []);
 
-    const turnCard = () => {
-        if (stageCards.length > 0) {
-            console.log("turning card: " + game.newGame.cardStack[0].color);
-            Rune.actions.updateCardStack()
-            setActiveCard(game.newGame.cardStack[0])
-            // console.log(game)
-        }
-    };
+  const turnCard = () => {
+    if (stageCards.length > 0) {
+      console.log("turning card: " + game.newGame.cardStack[0].color);
+      Rune.actions.updateCardStack();
+      setActiveCard(game.newGame.cardStack[0]);
+      // console.log(game)
+    }
+  };
 
-    return (
-        <div
-            id="stage"
-            className="flex w-full p-2 bg-orange-600 rounded-b-lg h-fit"
-        >
-            <div
-                id="active-card"
-                className={`mr-2 w-10 h-14 ${activeCard || "border-2 border-black border-dashed rounded-md"}`}
-            >
-                {activeCard && <StageCard color={activeCard.color} />}
-            </div>
-            <div
-                id="deck"
-                className="relative flex" // ... like me
-                onClick={() => {
-                    turnCard();
-                }}
-            >
-                {game.newGame.cardStack.slice(1).map((cardItem:Card, i:number) => (
-                    <div key={`stage-cards-${i}`}>
-                        <StageCard
-                            color={cardItem.color}
-                            leftOffset={`${i * 10 + 2}px`}
-                            z={`${stageCards.length - i}`}
-                            limbs={cardItem.limbs}
-                        />
-                    </div>
-                ))}
-            </div>
-        </div>
-    );
+  return (
+    <div
+      id="stage"
+      className="flex w-full p-4 bg-orange-600 rounded-xl h-fit"
+    >
+      <div
+        id="active-card"
+        className={`mr-16 w-10 h-14 ${
+          activeCard || "border-4 border-black border-dashed rounded-xl"
+        }`}
+        style={{ width: "20vw", height: "25vw" }}
+      >
+        {activeCard ? (
+          <StageCard color={activeCard.color} limbs={activeCard.limbs} />
+        ) : (
+          <> </>
+        )}
+      </div>
+      <div
+        id="deck"
+        className="relative flex" // ... like me
+        onClick={() => {
+          turnCard();
+        }}
+      >
+        {game.newGame.cardStack.map((cardItem: Card, i: number) => (
+          <div key={`stage-cards-${i}`}>
+            <StageCard
+              color={cardItem.color}
+              leftOffset={`${i * 10 + 2}px`}
+              z={`${stageCards.length - i}`}
+              limbs={cardItem.limbs}
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
--- src/components/StageCard.tsx
+++ src/components/StageCard.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 type StageCardProps = {
   color: string;
@@ -13,16 +13,24 @@ export const StageCard: React.FC<StageCardProps> = ({
   z,
   limbs,
 }) => {
+  const [colorNicer, setColorNicer] = useState<Record<string,string>>({
+    'red': 'bg-vivid-raspberry',
+    'yellow': 'bg-ronchi',
+    'green': 'bg-willpower-orange',
+    'blue': 'bg-blue-purple',
+    }) 
+  
   return (
     <>
       <div
         id="stage-card"
-        className="absolute w-10 h-14 border-2 border-black rounded-md"
-        style={{ backgroundColor: color, left: leftOffset, zIndex: z }}
+        className= {`absolute w-10 h-14 border-4 border-black rounded-xl ${colorNicer[color]}`}
+        style={{ left: leftOffset , zIndex: z, width: "20vw", height: "25vw" }}
       >
         <div className="bold">
           {limbs && limbs[0]}
           {limbs && limbs[1]}
+          <br />
           {limbs && limbs[2]}
           {limbs && limbs[3]}
           </div>
